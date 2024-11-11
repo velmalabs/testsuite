9ccbe62f7acb93c9a66fdce5acabd3d10ccc384c
Maikel ten Voorde <info@matevo.nl>
2024-11-11 13:56:43 +0100

    Better error handeling

--- src/suite/+page.ts
+++ src/suite/+page.ts
@@ -1,67 +1,69 @@
 // @ts-nocheck
-import {browser} from "$app/environment";
-import {writable} from "svelte/store";
-import {createRawSnippet, mount} from "svelte";
-import components from "@velmalabs/testsuite/.cache/components";
+import { browser } from '$app/environment';
+import { writable } from 'svelte/store';
+import { createRawSnippet, mount } from 'svelte';
+import components from '@velmalabs/testsuite/.cache/components';
 
 export function load() {
-    let response = writable({component: null, props: {}});
-    if (browser) {
-        window.render = (path: string, options: { props: {} }) => {
-            response.update(c => ({
-                ...c,
-                component: components[path],
-                props: deserializeProps(options?.props ?? {})
-            }));
-        }
-        if (window.renderLoaded) {
-            window.renderLoaded();
-        }
-    }
+	let response = writable({ component: null, props: {} });
+	if (browser) {
+		window.render = (path: string, options?: { props?: Record<string, unknown> }) => {
+			console.log(`render("${path}", ${JSON.stringify(options)})`);
+			if (!components[path]) {
+				throw new Error(`Component "${path}" does not exist`);
+			}
+			response.update((c) => ({
+				...c,
+				component: components[path],
+				props: deserializeProps(options?.props ?? {})
+			}));
+		};
+		if (window.renderLoaded) {
+			window.renderLoaded();
+		}
+	}
 
-    return response;
+	return response;
 }
-
 
 function deserializeProps(props: object) {
-    return Object.entries(props).reduce((acc, [key, value]) => {
-        if (typeof value === 'string' && value.startsWith('__FN__:')) {
-            acc[key] = (...args) => {
-                const functionString = value.slice(7);
-                return new Function('args', `return (${functionString})(...args);`)(args);
-            }
-        } else if (typeof value === 'object') {
-            if (value.type === 'snippet') {
-                acc[key] = renderSnippet(Object.values(value.components))
-            } else {
-                acc[key] = deserializeProps(value);
-            }
-        } else {
-            acc[key] = value;
-        }
-        return acc;
-    }, {});
+	return Object.entries(props).reduce((acc, [key, value]) => {
+		if (typeof value === 'string' && value.startsWith('__FN__:')) {
+			acc[key] = (...args) => {
+				const functionString = value.slice(7);
+				return new Function('args', `return (${functionString})(...args);`)(args);
+			};
+		} else if (typeof value === 'object') {
+			if (value.type === 'snippet') {
+				acc[key] = renderSnippet(Object.values(value.components));
+			} else {
+				acc[key] = deserializeProps(value);
+			}
+		} else {
+			acc[key] = value;
+		}
+		return acc;
+	}, {});
 }
 
-
 function renderSnippet(entries) {
-    return createRawSnippet(() => ({
-        render: () => `<div></div>`,
-        setup: (target) => {
-            for (const snippet of entries) {
-                const component = components[snippet.path];
-                if (!component) {
-                    throw new Error(`Component "${snippet.path}" does not exist`);
-                }
-                mount(component, {
-                    target,
-                    props: deserializeProps(snippet.props)
-                });
-            }
-            while (target.firstChild) {
-                target.parentNode.insertBefore(target.firstChild, target);
-            }
-            target.remove();
-        }
-    }))
+	return createRawSnippet(() => ({
+		render: () => `<div></div>`,
+		setup: (target) => {
+			for (const snippet of entries) {
+				const component = components[snippet.path];
+				if (!component) {
+					throw new Error(`Component "${snippet.path}" does not exist`);
+				}
+				mount(component, {
+					target,
+					props: deserializeProps(snippet.props)
+				});
+			}
+			while (target.firstChild) {
+				target.parentNode.insertBefore(target.firstChild, target);
+			}
+			target.remove();
+		}
+	}));
 }
--- src/velma.specs.js
+++ src/velma.specs.js
@@ -1,83 +1,105 @@
-import {test} from '@playwright/test';
+import { test } from '@playwright/test';
 
 export * from '@playwright/test';
 import dictionary from '../.cache/dictionary.js';
 
 export let page;
 export let testInfo;
-test.beforeAll(async ({browser}, info) => {
-    page = await browser.newPage();
-    testInfo = info;
-    await page.goto('http://localhost:4173/__testsuite__', {waitUntil: 'domcontentloaded'});
-    await page.exposeFunction('sandboxFN', (evHash) => _ctx[evHash]());
-    await page.waitForSelector('main', {state: 'attached'});
+export let takeScreenshot = false;
+
+test.beforeAll(async ({ browser }, info) => {
+	page = await browser.newPage();
+	testInfo = info;
+	await page.goto('http://localhost:4173/__testsuite__', { waitUntil: 'domcontentloaded' });
+	await page.exposeFunction('sandboxFN', (evHash) => _ctx[evHash]());
+	await page.waitForSelector('main', { state: 'attached' });
 });
 
-test.afterEach(async () => {
-    // Take a screenshot of the rendered component
-    const homeScreenshot = await page.screenshot();
-    await testInfo.attach('Screenshot', {
-        body: homeScreenshot, contentType: 'image/png',
-    });
+test.beforeEach(async () => {
+	takeScreenshot = false;
+});
+test.afterEach(async ({ page: _ }, testInfo) => {
+	if (takeScreenshot) {
+		const homeScreenshot = await page.screenshot();
+		await testInfo.attach('Screenshot', {
+			body: homeScreenshot,
+			contentType: 'image/png'
+		});
+	}
 });
 
 let _ctx = {};
 
 async function render(component, props) {
-    const path = locateComponent(component, 'render');
-    const main = await page.getByTestId('main');
-    await page.evaluate(async ({path, props}) => {
-        if (!window.render) {
-            await new Promise(resolve => {
-                window.renderLoaded = () => resolve();
-            });
-        }
-        window.render(path, {props});
-    }, {path, props: serializeProps(props)});
+	takeScreenshot = true;
+	const path = locateComponent(component, 'render');
+	const main = await page.getByTestId('main');
+	await page.evaluate(
+		async ({ path, props }) => {
+			if (!window.render) {
+				await new Promise((resolve) => {
+					window.renderLoaded = () => resolve();
+				});
+			}
+			window.render(path, { props });
+		},
+		{ path, props: serializeProps(props) }
+	);
 
-    return main;
+	return main;
 }
 
 function snippet(component, props) {
-    const components = Array.isArray(component) ? component : [{path: component, props}];
-    return {
-        type: 'snippet',
-        components: components.map(c => ({path: locateComponent(c.path, 'snippet'), props: serializeProps(c.props)}))
-    }
+	const components = Array.isArray(component) ? component : [{ path: component, props }];
+	return {
+		type: 'snippet',
+		components: components.map((c) => ({
+			path: locateComponent(c.path, 'snippet'),
+			props: serializeProps(c.props)
+		}))
+	};
 }
 
-
 function serializeProps(props) {
-    return Object.entries(props).reduce((acc, [key, value]) => {
-        if (typeof value === 'function') {
-            const evHash = value.toString().split('').reduce((a, b) => {
-                a = ((a << 5) - a) + b.charCodeAt(0);
-                return a & a
-            }, 0).toString(36).substr(0, 8);
-            _ctx[evHash] = value;
-            acc[key] = `__FN__:()=>window.sandboxFN("${evHash}")`;
-        } else if (typeof value === 'object') {
-            acc[key] = serializeProps(value);
-        } else {
-            acc[key] = value;
-        }
-        return acc;
-    }, {});
+	return Object.entries(props).reduce((acc, [key, value]) => {
+		if (typeof value === 'function') {
+			const evHash = value
+				.toString()
+				.split('')
+				.reduce((a, b) => {
+					a = (a << 5) - a + b.charCodeAt(0);
+					return a & a;
+				}, 0)
+				.toString(36)
+				.substr(0, 8);
+			_ctx[evHash] = value;
+			acc[key] = `__FN__:()=>window.sandboxFN("${evHash}")`;
+		} else if (typeof value === 'object') {
+			acc[key] = serializeProps(value);
+		} else {
+			acc[key] = value;
+		}
+		return acc;
+	}, {});
 }
 
 function locateComponent(component, type) {
-    const relativePath = backtraceFilePath(type).substring(1);
-    const relativeDir = relativePath.split('/').slice(0, -1).join('/');
-    return (relativeDir + '/' + component).replaceAll('/./', '/');
+	console.log(`locateComponent: ${component}`);
+	if (component.startsWith('/')) {
+		return component.substring(1);
+	}
+
+	const relativePath = backtraceFilePath(type).substring(1);
+	const relativeDir = relativePath.split('/').slice(0, -1).join('/');
+	return (relativeDir + '/' + component).replaceAll('/./', '/');
 }
 
 function backtraceFilePath(type = 'render') {
-    const rootPath = process.cwd();
-    const stack = new Error().stack;
-    const lines = stack.split('\n');
-    const line = lines.findIndex(l => l.includes(`at ${type} (`));
-    return lines[line+1].split(rootPath)[1].split(':')[0];
+	const rootPath = process.cwd();
+	const stack = new Error().stack;
+	const lines = stack.split('\n');
+	const line = lines.findIndex((l) => l.includes(`at ${type} (`));
+	return lines[line + 1].split(rootPath)[1].split(':')[0];
 }
 
-
-export {render, snippet, dictionary};
+export { render, snippet, dictionary };
